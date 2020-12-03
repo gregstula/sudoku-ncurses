c07fbada9ad92fd480d2faa8ea753377047e663b
Greg Stula <gregstula@me.com>
2020-12-03 14:47:48 -0500

    Print numbers

--- sudoku_game.hpp
+++ sudoku_game.hpp
@@ -1,4 +1,5 @@
 #include "window.hpp"
+#include <string>
 // initial drawing coordinates and dimensions for lines
 constexpr int hline_starty = 6;
 constexpr int hlline_startx = 1;
@@ -10,6 +11,18 @@ constexpr int vline_startx = 12;
 constexpr int vline_length = 6;
 
 struct sudoku_game {
+
+    char numbers[9][9] = {
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+        {'1', '2', '3', '4', '5', '6', '7', '8', '9'},
+    };
 
     curses::window main_win = curses::window((vline_length * 3) + 1, (hline_length * 3) + 1,0,0);
     bool is_running = true;
@@ -44,6 +57,13 @@ void sudoku_game::render() {
             for (int j = 0; j < 3; j++) {
                 main_win.print_hline(hline_starty + (hline_starty * i), hlline_startx + (hline_length * j), hline_length);
                 main_win.print_vline(vline_starty + (vline_length * i), vline_startx + (vline_startx * j), vline_length);
+            }
+        }
+
+        // print numbers
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                main_win.print_at_coords((j * 2) + 1, (i * 4) + 1, std::string(1,numbers[i][j]));
             }
         }
     }
